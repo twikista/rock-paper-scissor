0f38346ccd4edccc127d00f1c311e3b727b8f5ac
viro <twikista@gmail.com>
2021-08-10 22:15:32 +0100

    added pseudocode to app.js file guide my implementation of the project

---
+++ app.js
@@ -0,0 +1,17 @@
+//create an array to hold the random selections of the computer
+
+//create a function that randomly generates the computer's selection
+
+//create a function that determines the winner of each game round depending on player selection and computer selection
+
+//check if player selection is the same as computer selection
+
+//check if the player or computer wins if player selection is rock 
+
+//check if the player or computer wins if player selection is paper
+
+//check if the player or computer wins if player selection is scissors 
+
+// create a function that determines the number of game rounds and alert the winner to the browser window
+
+//call the game function to initialize the game
