1b9eab1af7bbf1495d7c54014838d129f927d249
viro <twikista@gmail.com>
2021-08-10 22:33:54 +0100

    defined the basic structure, including parameters of the function that each round of the game

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ function computerPlay(){
 
 //create a function that determines the winner of each game round depending on player selection and computer selection
 
+function playRound(playerSelection, computerSelection){
+    gameHeader(gameRound);
+    let playOutcome
+    playerScore = 0;
+    computerScore = 0;
+    const playerSelectionlowerCase = playerSelection.toLowerCase();
+    const tie = playerSelectionlowerCase === computerSelection;
+    console.log(`Player's selection: ${playerSelectionlowerCase}`);
+    console.log(`Computer's selection: ${computerSelection}`);
+}
+
 //check if player selection is the same as computer selection
 
 //check if the player or computer wins if player selection is rock 
