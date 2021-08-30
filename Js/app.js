8aadaeae3d718b3181e6388a9fa0a48925989856
viro <twikista@gmail.com>
2021-08-30 06:29:26 +0100

    grap various game elements from HTML to JavaScript

--- Js/app.js
+++ Js/app.js
@@ -2,6 +2,33 @@
 
 const options = ["rock", "paper", "scissors"];
 
+//grab the various section and buttons to each section from HTML
+const pagesBtns = document.querySelectorAll("[data-target]");
+const pages = document.querySelectorAll(".pages");
+
+//grab the play game buttons from HTML
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+
+//grab various HTML elements that make up game funtionality
+const playBtns = document.querySelector(".play-buttons");
+const playerPick = document.querySelector("#player-selection");
+const computerPick = document.querySelector("#computer-selection");
+const roundWinner = document.querySelector(".round-winner");
+const playerRunningScore = document.querySelector("#player-running-score");
+const computerRunningScore = document.querySelector("#computer-running-score");
+const displayGameWinner = document.querySelector(".display-game-winner");
+const gameWinner = document.querySelector(".game-winner");
+const replayGameBtn = document.querySelector(".play-again-btn");
+
+//grab game audios
+const winGame = document.querySelector(".win-game");
+const winRound = document.querySelector(".win-round");
+const GameOverSound = document.querySelector(".game-over-sound");
+const lossRoundTwo = document.querySelector(".lose-round");
+const tieRound = document.querySelector(".tie-round");
+
 //create variables to hold total scores
 let playerScore = 0;
 let computerScore = 0;
